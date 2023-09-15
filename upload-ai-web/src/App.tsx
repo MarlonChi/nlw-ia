0741852b1d4bca165c0caa739e78c3511946aa06
MarlonChi <marlonchiodelli@hotmail.com>
2023-09-15 11:13:12 -0300

    feat: upload.ai

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,7 +1,7 @@
 import { Separator } from "./components/ui/separator";
 import { Button } from "./components/ui/button";
 
-import { Github, FileVideo, Upload, Wand2, Wand } from "lucide-react";
+import { Github, FileVideo, Upload, Wand2 } from "lucide-react";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import {
